f6814cdb3dad8de328dc446f2bc09ca792b5afb6
mradulvarshney <mradulv26@gmail.com>
2023-08-30 23:43:27 +0530

    changes in resolution

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,73 +1,73 @@
 (function ($) {
-	
+
 	"use strict";
 
 	$('.owl-show-events').owlCarousel({
-		items:4,
-		loop:true,
+		items: 4,
+		loop: true,
 		dots: true,
 		nav: true,
 		autoplay: true,
-		margin:30,
-		  responsive:{
-			  0:{
-				  items:1
-			  },
-			  600:{
-				  items:2
-			  },
-			  1000:{
-				  items:4
-			  }
-		  }
-	  })
+		margin: 30,
+		responsive: {
+			0: {
+				items: 1
+			},
+			600: {
+				items: 2
+			},
+			1000: {
+				items: 4
+			}
+		}
+	})
 
 	const second = 1000,
-      minute = second * 60,
-      hour = minute * 60,
-      day = hour * 24;
+		minute = second * 60,
+		hour = minute * 60,
+		day = hour * 24;
 
 	let countDown = new Date('Mar 31, 2022 09:30:00').getTime(),
-    x = setInterval(function() {    
+		x = setInterval(function () {
 
-      let now = new Date().getTime(),
-          distance = countDown - now;
+			let now = new Date().getTime(),
+				distance = countDown - now;
 
-      document.getElementById('days').innerText = Math.floor(distance / (day)),
-        document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
-        document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
-        document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
+			document.getElementById('days').innerText = Math.floor(distance / (day)),
+				document.getElementById('hours').innerText = Math.floor((distance % (day)) / (hour)),
+				document.getElementById('minutes').innerText = Math.floor((distance % (hour)) / (minute)),
+				document.getElementById('seconds').innerText = Math.floor((distance % (minute)) / second);
 
-      //do something later when date is reached
-      //if (distance < 0) {
-      //  clearInterval(x);
-      //  'IT'S MY BIRTHDAY!;
-      //}
+			//do something later when date is reached
+			//if (distance < 0) {
+			//  clearInterval(x);
+			//  'IT'S MY BIRTHDAY!;
+			//}
 
-    }, second)
+		}, second)
 
-	$(function() {
-        $("#tabs").tabs();
-    });
-	
+	$(function () {
+		$("#tabs").tabs();
+	});
 
-	$('.schedule-filter li').on('click', function() {
-        var tsfilter = $(this).data('tsfilter');
-        $('.schedule-filter li').removeClass('active');
-        $(this).addClass('active');
-        if (tsfilter == 'all') {
-            $('.schedule-table').removeClass('filtering');
-            $('.ts-item').removeClass('show');
-        } else {
-            $('.schedule-table').addClass('filtering');
-        }
-        $('.ts-item').each(function() {
-            $(this).removeClass('show');
-            if ($(this).data('tsmeta') == tsfilter) {
-                $(this).addClass('show');
-            }
-        });
-    });
+
+	$('.schedule-filter li').on('click', function () {
+		var tsfilter = $(this).data('tsfilter');
+		$('.schedule-filter li').removeClass('active');
+		$(this).addClass('active');
+		if (tsfilter == 'all') {
+			$('.schedule-table').removeClass('filtering');
+			$('.ts-item').removeClass('show');
+		} else {
+			$('.schedule-table').addClass('filtering');
+		}
+		$('.ts-item').each(function () {
+			$(this).removeClass('show');
+			if ($(this).data('tsmeta') == tsfilter) {
+				$(this).addClass('show');
+			}
+		});
+	});
 
 
 	// Window Resize Mobile Menu Fix
@@ -76,11 +76,11 @@
 
 	// Scroll animation init
 	window.sr = new scrollReveal();
-	
+
 
 	// Menu Dropdown Toggle
-	if($('.menu-trigger').length){
-		$(".menu-trigger").on('click', function() {	
+	if ($('.menu-trigger').length) {
+		$(".menu-trigger").on('click', function () {
 			$(this).toggleClass('active');
 			$('.header-area .nav').slideToggle(200);
 		});
@@ -88,15 +88,15 @@
 
 
 	// Page loading animation
-	 $(window).on('load', function() {
+	$(window).on('load', function () {
 
-        $('#js-preloader').addClass('loaded');
+		$('#js-preloader').addClass('loaded');
 
-    });
+	});
 
 
 	// Window Resize Mobile Menu Fix
-	$(window).on('resize', function() {
+	$(window).on('resize', function () {
 		mobileNav();
 	});
 
@@ -104,8 +104,8 @@
 	// Window Resize Mobile Menu Fix
 	function mobileNav() {
 		var width = $(window).width();
-		$('.submenu').on('click', function() {
-			if(width < 767) {
+		$('.submenu').on('click', function () {
+			if (width < 767) {
 				$('.submenu ul').removeClass('active');
 				$(this).find('ul').toggleClass('active');
 			}
@@ -114,3 +114,20 @@
 
 
 })(window.jQuery);
+
+const detectDeviceType = () =>
+	/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent)
+		? 'Mobile'
+		: 'Desktop';
+const device = detectDeviceType();
+console.log(device);
+
+if(device == "Mobile")
+{
+	const link = document.querySelector(".sub-footer .whatsapp");
+	link.href = 'https://wa.me/917977260253/';
+}
+else{
+	const link = document.querySelector(".sub-footer .whatsapp");
+	link.href = 'https://web.whatsapp.com/send?phone=917977260253';	
+}
